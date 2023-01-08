1389a7d6f15e9312e258b68ab1f92de541415adf
w1710n0 <wijiono@gmail.com>
2023-01-08 13:59:52 +0000

    bananadroidify

--- banana_sakura.mk
+++ banana_sakura.mk
@@ -9,15 +9,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common BananaDroid stuff.
+$(call inherit-product, vendor/banana/config/common.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_BOOTANIMATION_HALF_RES := true
+BANANA_MAINTAINER := w1710n0
+TARGET_SUPPORTS_QUICK_TAP := true
 
 # Inherit from sakura device
 $(call inherit-product, device/xiaomi/sakura/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := sakura
-PRODUCT_NAME := lineage_sakura
+PRODUCT_NAME := banana_sakura
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 6 Pro
